8a2e007a5f1f6bd6acfaf1eadff2d59e3efae410
David Kumarbai <cygnidavid@gmail.com>
2018-05-09 15:43:59 -0700

    Adds roman numeral

---
+++ algorithms/RomanNumerals/FromRoman/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func FromRoman(num string) int {
+	romans := map[string]int{
+		"M": 1000,
+		"D": 500,
+		"C": 100,
+		"L": 50,
+		"X": 10,
+		"V": 5,
+		"I": 1,
+	}
+
+	n := 0
+
+	for i, val := range num {
+		var next int
+		cur := romans[string(val)]
+		if i != len(num)-1 {
+			next = romans[string(num[i+1])]
+		}
+
+		if cur < next {
+			n -= cur
+		} else {
+			n += cur
+		}
+	}
+
+	return n
+}
+
+func main() {
+	t1 := FromRoman("X")
+	t2 := FromRoman("IX")
+	t3 := FromRoman("MXII")
+	t4 := FromRoman("MIX")
+
+	fmt.Printf("Test 1: %d\n", t1)
+	fmt.Printf("Test 2: %d\n", t2)
+	fmt.Printf("Test 3: %d\n", t3)
+	fmt.Printf("Test 4: %d\n", t4)
+}
