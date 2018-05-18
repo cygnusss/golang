6f5d7a368bddde6ad23d3ac48fca00ab65228e4d
David Kumarbai <cygnidavid@gmail.com>
2018-05-18 15:13:28 -0700

    data-structures/binary-trees/binary-heap: Adds correct insertion; passes the tests

---
+++ data-structures/binary-trees/binary-heap/binaryHeap.go
@@ -0,0 +1,46 @@
+package binaryHeap
+
+import (
+	"fmt"
+)
+
+type BinaryHeapMethods interface {
+	Insert(item int)
+	GetParentIndex(i int) int
+}
+
+type BinaryHeap struct {
+	Nodes []int
+}
+
+func (b *BinaryHeap) Peek() int {
+	return b.Nodes[0]
+}
+
+func (b *BinaryHeap) Insert(item int) {
+	b.Nodes = append(b.Nodes, item)
+	i := len(b.Nodes) - 1
+	p := b.GetParentIndex(i)
+
+	for b.Nodes[i] < b.Nodes[p] {
+		b.Nodes[i], b.Nodes[p] = b.Nodes[p], b.Nodes[i]
+		i = p
+		p = b.GetParentIndex(i)
+	}
+}
+
+func (b *BinaryHeap) GetParentIndex(i int) int {
+	return (i - 1) / 2
+}
+
+func (b *BinaryHeap) HeapifyUp(i int) {
+
+}
+
+func (b *BinaryHeap) HeapifyDown(i int) {
+
+}
+
+func main() {
+	fmt.Println("haha")
+}
---
+++ data-structures/binary-trees/binary-heap/binaryHeap_test.go
@@ -0,0 +1,51 @@
+package binaryHeap
+
+import (
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	bh := BinaryHeap{[]int{}}
+	bh.Insert(0)
+
+	if len(bh.Nodes) == 0 {
+		t.Error("Binary Heap isn't inserting items")
+	}
+}
+
+func TestGetParentIndex(t *testing.T) {
+	bh := BinaryHeap{[]int{}}
+
+	zero := bh.GetParentIndex(0)
+	one := bh.GetParentIndex(1)
+	eight := bh.GetParentIndex(8)
+
+	if zero != 0 {
+		t.Error("Doesn't handle top element")
+	}
+
+	if one != 0 {
+		t.Error("Doesn't hanlde odd children")
+	}
+
+	if eight != 3 {
+		t.Error("Doesn't hanlde even children")
+	}
+}
+func TestCorrectInsertion(t *testing.T) {
+	bh := BinaryHeap{[]int{}}
+
+	for i := 10; i > 0; i-- {
+		bh.Insert(i)
+	}
+
+	for i := 0; i < len(bh.Nodes); i++ {
+		p := bh.GetParentIndex(i)
+		parent := bh.Nodes[p]
+		child := bh.Nodes[i]
+
+		if parent > child {
+			t.Error("Parent is greater than child!")
+		}
+	}
+}
